76e4a6a5939ce279e9495f7f6107c394612c8849
irasakumarvijayan <irasakumarvijayan@gmail.com>
2021-05-24 09:14:32 +0800

    Updated code to Get Method

--- src/main/java/com/kalsym/report/service/model/repository/OrderItemRepository.java
+++ src/main/java/com/kalsym/report/service/model/repository/OrderItemRepository.java
@@ -12,10 +12,10 @@ import java.util.List;
 public interface OrderItemRepository extends JpaRepository<OrderItem, String> {
     List<OrderItem> findAllByOrderId (String orderId);
 
-//    @Query(value = "SELECT ot.productId, count(productId)  FROM symplified.order_item ot INNER JOIN symplified.order o ON o.id = ot.orderId WHERE created > :date1" +
-//            " AND created < :date2 AND storeId = :storeId and o.paymentStatus= :status GROUP BY ot.productId ORDER BY count(ot.productId) DESC LIMIT :limit ;", nativeQuery = true)
     @Query(value = "SELECT ot.productId, count(productId)  FROM symplified.order_item ot INNER JOIN symplified.order o ON o.id = ot.orderId WHERE created > :date1" +
-            " AND created < :date2  GROUP BY ot.productId ORDER BY count(ot.productId) DESC LIMIT :limit ;", nativeQuery = true)
+            " AND created < :date2 AND storeId = :storeId and o.paymentStatus= :status GROUP BY ot.productId ORDER BY count(ot.productId) DESC LIMIT :limit ;", nativeQuery = true)
+//    @Query(value = "SELECT ot.productId, count(productId)  FROM symplified.order_item ot INNER JOIN symplified.order o ON o.id = ot.orderId WHERE created > :date1" +
+//            " AND created < :date2  GROUP BY ot.productId ORDER BY count(ot.productId) DESC LIMIT :limit ;", nativeQuery = true)
     List<Object[]> findAllByTopSaleProduct(@Param("date1") String date1, @Param("date2") String date2, @Param("storeId") String storeId, @Param("status") String status, @Param("limit") Integer limit);
 //    List<Object[]> findAllByTopSaleProduct(@Param("date1") String date1, @Param("date2") String date2, @Param("limit") Integer limit);
 }
