b6fc07b7b1dbd2cee5c4ad9afd935296d5d686ed
sarosh <sarosh.tariq@kalsym.com>
2021-06-08 16:26:23 +0500

    bug fix

---
+++ src/main/java/com/kalsym/report/service/model/report/StoreDailySaleIdentity.java
@@ -0,0 +1,58 @@
+package com.kalsym.report.service.model.report;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ *
+ * @author Sarosh
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class StoreDailySaleIdentity implements Serializable {
+
+    private Date date;
+    private String storeId;
+
+    public StoreDailySaleIdentity(Date date, String storeId) {
+        this.date = date;
+        this.storeId = storeId;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 97 * hash + Objects.hashCode(this.date);
+        hash = 97 * hash + Objects.hashCode(this.storeId);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final StoreDailySaleIdentity other = (StoreDailySaleIdentity) obj;
+        if (!Objects.equals(this.date, other.date)) {
+            return false;
+        }
+        if (!Objects.equals(this.storeId, other.storeId)) {
+            return false;
+        }
+        return true;
+    }
+
+}
